6d7bb36806cbb9d1d4f2ea500c4800b6c2f84547
tinfungster <70585550+tinfungster@users.noreply.github.com>
2021-01-04 19:17:37 +0000

    Create HittingInaccurateText.py

---
+++ HittingInaccurateText.py
@@ -0,0 +1,23 @@
+# How to Hit Inaccurate Text with a Vector (Arrow) !
+
+from manimlib.imports import *
+
+class Test(Scene):
+    def construct(self):
+        vector = Vector(3 * RIGHT)
+        runtime = 5
+        parts = 300
+        part = 0
+
+        theta = 0
+
+        while part <= parts:
+            self.play(Rotating(vector, radians=TAU/parts, about_point=ORIGIN), run_time=5/300)
+            text = TexMobject("e^{i" + str(theta) + "}")
+            text.add_updater(lambda t: t.next_to(vector, RIGHT, buff=0.05))
+            self.add(text)
+            part += 1
+
+        self.wait()
+
+# This is a failed attempt of code for rotating circles and arrows for my Fourier Series Explanation video.
